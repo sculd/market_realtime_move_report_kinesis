9f6ebec3f2a46e0318f12fb5560bd88bb04dcce9
Hyungjun Lim <sculd3@gmail.com>
2021-12-18 21:05:44 -0500

    slack polygon binance:

--- src/main/java/com/marketsignal/publish/SlackPublisher.java
+++ src/main/java/com/marketsignal/publish/SlackPublisher.java
@@ -22,19 +22,33 @@ public class SlackPublisher {
         return anomaly.changeThreshold > 0.05;
     }
 
-    public void publish(ChangesAnomaly.Anomaly anomaly) {
+    private void publishToChannel(ChangesAnomaly.Anomaly anomaly, final String channel) {
         String token = System.getenv("SLACK_TOKEN");
-        String channel = System.getenv("SLACK_CHANNEL");
 
         log.info("[SlackPublisher] publishing a new anomaly: {}", anomaly.toString());
         try {
             ChatPostMessageResponse response = slack.methods(token).chatPostMessage(req -> req
                     .channel(channel) // Channel ID
                     .text(anomaly.toString()));
+            if (!response.isOk()) {
+                log.error(response.getMessage().toString());
+            }
         } catch (IOException ex) {
             log.error(ex.getMessage());
         } catch (SlackApiException ex) {
             log.error(ex.getMessage());
+        }
+    }
+
+    public void publish(ChangesAnomaly.Anomaly anomaly) {
+        String channel = System.getenv("SLACK_CHANNEL");
+        String channel_polygon_stock = System.getenv("SLACK_CHANNEL_POLYGON_STOCK");
+        String channel_binance = System.getenv("SLACK_CHANNEL_BINANCE");
+        publishToChannel(anomaly, channel);
+        if (anomaly.market == "polygon") {
+            publishToChannel(anomaly, channel_polygon_stock);
+        } else if (anomaly.market == "binance") {
+            publishToChannel(anomaly, channel_binance);
         }
     }
 }
