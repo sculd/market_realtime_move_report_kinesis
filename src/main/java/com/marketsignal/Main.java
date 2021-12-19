f5b6f2c78379c0732e5ae70c458eb8c812e24ed6
Hyungjun Lim <sculd3@gmail.com>
2021-12-18 23:35:56 -0500

    Main class read env file

--- src/main/java/com/marketsignal/Main.java
+++ src/main/java/com/marketsignal/Main.java
@@ -6,12 +6,39 @@ import com.marketsignal.publish.SlackPublisher;
 import com.marketsignal.timeseries.analysis.Changes;
 import com.marketsignal.timeseries.analysis.ChangesAnomaly;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.Map;
+import java.util.stream.Stream;
 
 
 public class Main {
+    static void setEnv(String key, String value) {
+        try {
+            Map<String, String> env = System.getenv();
+            Class<?> cl = env.getClass();
+            Field field = cl.getDeclaredField("m");
+            field.setAccessible(true);
+            Map<String, String> writableEnv = (Map<String, String>) field.get(env);
+            writableEnv.put(key, value);
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to set environment variable", e);
+        }
+    }
+
     public static void main(String[] args)
     {
+        String envVarFile = "k8s/secrets/envvar.env";
+        try (Stream<String> lines = Files.lines(Paths.get(envVarFile), Charset.defaultCharset())) {
+            lines.forEachOrdered(line -> setEnv(line.split("=")[0], line.split("=")[1]));
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+
         // for sandbox testing.
         DynamoDbPublisher dynamoDbPublisher = new DynamoDbPublisher();
         ChangesAnomaly.Anomaly anomaly = ChangesAnomaly.Anomaly.builder()
