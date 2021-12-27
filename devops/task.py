130bf7045e58ac38bb2ec09cb9f0ad9328f51a27
Hyungjun Lim <sculd3@gmail.com>
2021-12-27 11:51:53 -0500

    dev ops bug fix

--- devops/task.py
+++ devops/task.py
@@ -46,3 +46,7 @@ def start_task(group_name, container_name, task_definition, command):
     }
   )
   return str(response)
+
+start_task('market_realtime_move_report_kinesis', 'market_realtime_move_report_kinesis', 'market_realtime_move_report_kinesis:2',
+          ['java', '-jar', 'market_realtime_move_report_kinesis-1.0-SNAPSHOT.jar', '--shardid=0', '--apptype=changes_anomaly_stream'])
+
