7e125bd1eacfac55502a7b0571dc916afa84d47f
Hyungjun Lim <sculd3@gmail.com>
2022-05-11 19:31:25 -0400

    cache MarginAssetBinance

--- src/main/java/com/marketsignalbinance/marginasset/MarginAssetBinance.java
+++ src/main/java/com/marketsignalbinance/marginasset/MarginAssetBinance.java
@@ -5,19 +5,46 @@ import com.marketapi.binance.response.MarginAssetResponse;
 import com.marketsignal.marginasset.MarginAsset;
 import com.tradingbinance.state.BinanceUtil;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
 import java.util.Arrays;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class MarginAssetBinance implements MarginAsset {
     Gson gson = new Gson();
+    LoadingCache<String, Boolean> cached;
 
-    public boolean isMarginAsset(String pair) {
+    public MarginAssetBinance() {
+        cached = CacheBuilder.newBuilder()
+                .expireAfterAccess(10, TimeUnit.MINUTES)
+                .build(
+                        new CacheLoader<String, Boolean>() {
+                            public Boolean load(String pair) { // no checked exception
+                                return queryIsMarginAsset(pair);
+                            }
+                        });
+    }
+
+    boolean queryIsMarginAsset(String pair) {
         String[] tokens = pair.split("USD");
         String symbol = tokens[0];
         String result = BinanceUtil.client.createMargin().allAssets();
         MarginAssetResponse[] marginAssets = gson.fromJson(result, MarginAssetResponse[].class);
         Set<String> assets = Arrays.stream(marginAssets).map(mr -> mr.assetName).collect(Collectors.toSet());
         return assets.contains(symbol);
+    }
+
+    public boolean isMarginAsset(String pair) {
+        try {
+            return cached.get(pair);
+        } catch (ExecutionException e) {
+            // retry
+            return queryIsMarginAsset(pair);
+        }
     }
 }
--- src/main/java/com/marketsignalbinance/marginasset/Sandbox.java
+++ src/main/java/com/marketsignalbinance/marginasset/Sandbox.java
@@ -6,6 +6,7 @@ public class Sandbox {
     public static void main(String... args) {
         MarginAsset ma = new MarginAssetBinance();
         System.out.println(String.format("%s: %b", "BTCUSDT", ma.isMarginAsset("BTCUSDT")));
+        System.out.println(String.format("%s: %b", "BTCUSDT", ma.isMarginAsset("BTCUSDT")));
         System.out.println(String.format("%s: %b", "DUMMYUSDT", ma.isMarginAsset("DUMMYUSDT")));
     }
 }
