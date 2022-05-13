ba8f1a53ba955db70a44276e3f8fe4933e4c267a
Hyungjun Lim <sculd3@gmail.com>
2022-05-12 22:28:21 -0400

    binance margin response cached as whole instead of per pair

--- src/main/java/com/marketsignalbinance/marginasset/MarginAssetBinance.java
+++ src/main/java/com/marketsignalbinance/marginasset/MarginAssetBinance.java
@@ -17,34 +17,40 @@ import java.util.stream.Collectors;
 
 public class MarginAssetBinance implements MarginAsset {
     Gson gson = new Gson();
-    LoadingCache<String, Boolean> cached;
+    LoadingCache<String, String> cachedAPIResult;
+
+    final String API_RESULT_KEY = "all";
 
     public MarginAssetBinance() {
-        cached = CacheBuilder.newBuilder()
+        cachedAPIResult = CacheBuilder.newBuilder()
                 .expireAfterAccess(10, TimeUnit.MINUTES)
                 .build(
-                        new CacheLoader<String, Boolean>() {
-                            public Boolean load(String pair) { // no checked exception
-                                return queryIsMarginAsset(pair);
+                        new CacheLoader<String, String>() {
+                            public String load(String key) { // no checked exception
+                                return requestAllAssets();
                             }
                         });
     }
 
-    boolean queryIsMarginAsset(String pair) {
-        String[] tokens = pair.split("USD");
-        String symbol = tokens[0];
-        String result = BinanceUtil.client.createMargin().allAssets();
-        MarginAssetResponse[] marginAssets = gson.fromJson(result, MarginAssetResponse[].class);
-        Set<String> assets = Arrays.stream(marginAssets).map(mr -> mr.assetName).collect(Collectors.toSet());
-        return assets.contains(symbol);
+    String requestAllAssets() {
+        return BinanceUtil.client.createMargin().allAssets();
+    }
+
+    String getAllMarginAssetsResponse() {
+        try {
+            return cachedAPIResult.get(API_RESULT_KEY);
+        } catch (ExecutionException e) {
+            // retry
+            return requestAllAssets();
+        }
     }
 
     public boolean isMarginAsset(String pair) {
-        try {
-            return cached.get(pair);
-        } catch (ExecutionException e) {
-            // retry
-            return queryIsMarginAsset(pair);
-        }
+        String result = getAllMarginAssetsResponse();
+        MarginAssetResponse[] marginAssets = gson.fromJson(result, MarginAssetResponse[].class);
+        Set<String> assets = Arrays.stream(marginAssets).map(mr -> mr.assetName).collect(Collectors.toSet());
+        String[] tokens = pair.split("USD");
+        String symbol = tokens[0];
+        return assets.contains(symbol);
     }
 }
